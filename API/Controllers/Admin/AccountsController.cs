d0bee57976bedf748b74e30a3f18920b10331d02
Siddhant Rath <sid@tamu.edu>
2021-03-22 15:00:56 -0500

    Admin/Account controller list users and modify user

--- API/Controllers/Admin/AccountsController.cs
+++ API/Controllers/Admin/AccountsController.cs
@@ -31,6 +31,41 @@ namespace API.Controllers.Admin
       return Ok(formattedUsers);
     }
 
+
+    [HttpPost]
+    public async Task<ActionResult> createUser(API.DTOs.RegisterDto newUser)
+    {
+
+      // check if user exists
+      var exists = await _userManager.FindByEmailAsync(newUser.Email);
+      if (exists != null)
+      {
+        return BadRequest("User already exists");
+      }
+
+      // add the user
+
+      var user = new AppUser
+      {
+        DisplayName = newUser.DisplayName,
+        Email = newUser.Email,
+        UserName = newUser.Email
+      };
+
+      var result = await _userManager.CreateAsync(user);
+
+      if (!result.Succeeded)
+      {
+        return UnprocessableEntity("Failed to add new user");
+      }
+      result = await _userManager.AddToRolesAsync(user, newUser.Roles);
+      if (!result.Succeeded)
+      {
+        return UnprocessableEntity("Failed to set roles for new user");
+      }
+      return Ok();
+    }
+
     [HttpPost("{id}")]
     public async Task<ActionResult> updateUser(string id, API.DTOs.UserDto updatedUser)
     {
@@ -44,6 +79,10 @@ namespace API.Controllers.Admin
       }
       // prevent admin from de promoting himself
       var userEmailFromToken = HttpContext.User.FindFirstValue(ClaimTypes.Email);
+      if (updatedUser.Email == userEmailFromToken && !updatedUser.Roles.Contains("admin"))
+      {
+        return BadRequest("Admin cannot remove itself");
+      }
       if (updatedUser.Email != userEmailFromToken || updatedUser.Roles.Contains("admin"))
       {
         await _userManager.RemoveFromRolesAsync(user, roles);
@@ -52,6 +91,8 @@ namespace API.Controllers.Admin
       await _userManager.UpdateAsync(user);
       return Ok();
     }
+
+
 
 
 
