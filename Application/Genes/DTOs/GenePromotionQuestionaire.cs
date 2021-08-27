37c64160dce456b85060687650d844493341e4a3
sidxz <rath.siddhant@outlook.com>
2021-08-26 19:08:57 -0500

    Gene Promotion Submissions

--- Application/Genes/DTOs/GenePromotionQuestionaire.cs
+++ Application/Genes/DTOs/GenePromotionQuestionaire.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 
 namespace Application.Genes.DTOs
 {
-    public class Answer {
-        public string AnswerValue { get; set; }
-        public string AnswerDescription { get; set; }
-    }
-    public class GenePromotionQuestionaire
-    {
-        public Guid GeneID { get; set; }
-        public Dictionary<string, Answer> Answers { get; set; }
-    }
+  public class Answer
+  {
+    public string AnswerValue { get; set; }
+    public string AnswerDescription { get; set; }
+
+  }
+  public class GenePromotionQuestionaire
+  {
+    public Guid GeneID { get; set; }
+    public string SubmittedBy { get; set; }
+    public string Status { get; set; }
+    public Dictionary<string, Answer> Answers { get; set; }
+  }
 }
