184d5a394fc2e54b1c3b48af20a39dc7861c7314
panda-sas <panda.sas20@gmail.com>
2021-09-14 15:48:29 -0500

    Created Screen Details controller

--- API/Controllers/General/Screen/ScreenController.cs
+++ API/Controllers/General/Screen/ScreenController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,12 @@ namespace API.Controllers.General
     public async Task<IActionResult> ListScreens()
     {
       return HandleResult(await Mediator.Send(new Application.Screens.List.Query()));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetActivity(Guid id)
+    {
+      return HandleResult(await Mediator.Send(new Application.Screens.Details.Query { Id = id }));
     }
   }
 }
