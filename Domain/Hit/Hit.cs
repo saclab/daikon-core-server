680a89be12f24c1d6e6ec2d494d3b01357379376
sidxz <rath.siddhant@outlook.com>
2021-09-14 11:05:40 -0500

    added hit domain

---
+++ Domain/Hit/Hit.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Domain.Hit
+{
+  public class Hit
+  {
+    public Guid Id { get; set; }
+    public Target BaseScreen { get; set; }
+    public Guid ScreenId { get; set; }
+    public string Library { get; set; }
+    public string CompoundId { get; set; }
+    public string EnzymeActivity { get; set; }
+    public string Method { get; set; }
+    public string MIC { get; set; }
+    public string Structure { get; set; }
+    public string ClusterGroup { get; set; }
+    
+  }
+}
