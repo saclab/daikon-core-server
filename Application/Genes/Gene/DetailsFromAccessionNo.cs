73f6a715653980208f1805750fb7d03803158a3c
sidxz <rath.siddhant@outlook.com>
2021-09-29 12:53:24 -0500

    target importer

---
+++ Application/Genes/Gene/DetailsFromAccessionNo.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Errors;
+using Application.Genes.DTOs;
+using AutoMapper;
+//using Application.Errors;
+using Domain;
+using MediatR;
+using Persistence;
+
+// Entity framework core is required for 'include'
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Application.Genes
+{
+  public class DetailsFromAccessionNo
+  {
+    public class Query : IRequest<Result<GeneViewDTO>>
+    {
+      public string AccessionNumber { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, Result<GeneViewDTO>>
+    {
+      private readonly DataContext _context;
+      private readonly IMapper _mapper;
+      public Handler(DataContext context, IMapper mapper)
+      {
+        _mapper = mapper;
+        _context = context;
+
+      }
+      public async Task<Result<GeneViewDTO>> Handle(Query request, CancellationToken cancellationToken)
+      {
+        var gene = await _context.Genes
+        .Include(p => p.GenePublicData)
+        .Include(p => p.GeneNonPublicData)
+        .FirstOrDefaultAsync(g => g.AccessionNumber == request.AccessionNumber);
+      
+        var genesToReturn = _mapper.Map<GeneViewDTO>(gene);
+        return Result<GeneViewDTO>.Success(genesToReturn);
+      }
+    }
+  }
+}
