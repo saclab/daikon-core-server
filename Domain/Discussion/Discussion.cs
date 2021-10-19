5cc128f83194c82e5cf5527706ac991c1df9aa75
sidxz <rath.siddhant@outlook.com>
2021-10-19 12:16:56 -0500

    added discussion domain

---
+++ Domain/Discussion/Discussion.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+  public class Discussion
+  {
+    public Guid Id { get; set; }
+    public string Reference { get; set; }
+    public string Section { get; set; }
+    public string Topic { get; set; }
+    public string PostedBy { get; set; }
+    public DateTime Timestamp { get; set; }
+    public string Mentions { get; set; }
+    public string Tags { get; set; }
+    public List<Reply> Replies { get; set; }
+
+  }
+}
