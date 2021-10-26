167d2bc95848b43b836e5751d1dc4365fd33f3ec
sidxz <rath.siddhant@outlook.com>
2021-10-26 01:19:25 -0500

    Add user

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -1,19 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Domain;
 
 namespace API.DTOs
 {
-    public class UserDto
-    {   
-        public string Id { get; set; }
-        
-        public string DisplayName { get; set; }
+  public class UserDto
+  {
+    public string Id { get; set; }
 
-        [Required]
-        public string Email { get; set; }
+    public string DisplayName { get; set; }
 
-        public IList<string> Roles { get; set; }
+    [Required]
+    public string Email { get; set; }
 
-    }
+    public IList<string> Roles { get; set; }
+    public string Bio { get; set; }
+    public AppOrg Org { get; set; }
+    public bool Lock { get; set; }
+    public DateTime Archived { get; set; }
+
+  }
 }
