2b3359e9226afec45fa806eb31c56d13e3838c06
sidxz <rath.siddhant@outlook.com>
2021-10-27 03:22:33 -0500

    hits import

--- API/Controllers/General/Screen/HitController.cs
+++ API/Controllers/General/Screen/HitController.cs
@@ -6,7 +6,7 @@ namespace API.Controllers.General
     public class HitController : BaseApiController
     {
         [HttpPost]
-        public async Task<IActionResult> CreateHit(Domain.Hit newHit)
+        public async Task<IActionResult> CreateHit(Application.Screens.ValidatedHits.DTOs.HitCSVDTO newHit)
         {
             return HandleResult(await Mediator.Send(new Application.Screens.ValidatedHits.Create.Command { NewHit = newHit }));
         }
