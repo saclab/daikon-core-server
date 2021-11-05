f85bc30c9da12de7a067df2456bb7b39c84eb685
Siddhant Rath <sid@tamu.edu>
2021-11-05 15:50:39 -0500

    Domain : Gene GeneUnpublishedStructures

--- Domain/Gene/GeneUnpublishedStructures.cs
+++ Domain/Gene/GeneUnpublishedStructures.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Domain.Gene
+namespace Domain
 {
-    public class GeneUnpublishedStructures
-    {
-        
-    }
+  public class GeneUnpublishedStructures
+  {
+    public Guid Id { get; set; }
+    public Guid GeneId { get; set; }
+    public string GeneAccessionNumber { get; set; }
+    public string Organization { get; set; }
+    public string Method { get; set; }
+    public string Resolution { get; set; }
+    public string Condition { get; set; }
+    public string Ligand { get; set; }
+  }
 }
