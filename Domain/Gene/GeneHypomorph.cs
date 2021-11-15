fb781f8199d75641646103845a4aefb84449d957
Siddhant Rath <sid@tamu.edu>
2021-11-15 12:56:47 -0600

    gene hypomorphs entity added

---
+++ Domain/Gene/GeneHypomorph.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+  public class GeneHypomorph
+  {
+    public Guid Id { get; set; }
+    public Guid GeneId { get; set; }
+    public string GeneAccessionNumber { get; set; }
+    public string KnockdownStrain { get; set; }
+    public string Phenotype { get; set; }
+  }
+}
