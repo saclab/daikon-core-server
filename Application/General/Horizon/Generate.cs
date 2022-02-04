9fdb2943b604e472a3f89aafa602835dfff9492c
Siddhant Rath <sid@tamu.edu>
2022-02-03 22:44:49 -0600

    Include accession nos in horizon protein-complex

--- Application/General/Horizon/Generate.cs
+++ Application/General/Horizon/Generate.cs
@@ -75,6 +75,13 @@ namespace Application.General.Horizon
         if (target.Type == "protein-complex")
         {
           horizonRoot.Name = "GeneGroup";
+          var accessionNumbers = new List<string>();
+          foreach (var gene in target.TargetGenes)
+          {
+            accessionNumbers.Add(gene.AccessionNumber);
+          }
+
+          horizonRoot.Attributes.AccessionNumbers = accessionNumbers;
         }
 
         var horizonTarget = new HorizonTarget
