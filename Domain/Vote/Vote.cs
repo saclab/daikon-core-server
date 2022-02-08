9e7290cf88a0c0ccd2767987adb05f46f2a2c9cb
Siddhant Rath <sid@tamu.edu>
2022-02-08 15:21:52 -0600

    Voter Domain

---
+++ Domain/Vote/Vote.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Vote
+{
+  public class Vote
+  {
+    public Guid Id { get; set; }
+    public Guid ElementId { get; set; }
+    public int Positive { get; set; }
+    public int Neutral { get; set; }
+    public int Negative { get; set; }
+    public bool IsVotingAllowed { get; set; }
+    public DateTime LastModified { get; set; }
+  }
+}
