67ab3cbf7170fb22a45b0ea9e20aeafa19536f1f
Siddhant Rath <sid@tamu.edu>
2022-02-08 12:11:45 -0600

    Dockerfile.dev

--- Application/General/Horizon/Generate.cs
+++ Application/General/Horizon/Generate.cs
@@ -74,7 +74,7 @@ namespace Application.General.Horizon
 
         if (target.Type == "protein-complex")
         {
-          horizonRoot.Name = "GeneGroup";
+          horizonRoot.Name = "ProteinComplex";
           var accessionNumbers = new List<string>();
           foreach (var gene in target.TargetGenes)
           {
