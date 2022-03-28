35f4171e7c6974576d5da389fae0e68dac00cab2
Siddhant Rath <sid@tamu.edu>
2022-03-28 18:12:29 +0530

    P1

---
+++ Application/Projects/DTOs/P1DTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Projects.DTOs
+{
+  public class P1DTO
+  {
+    public Guid Id { get; set; }
+    public DateTime P1Start { get; set; }
+    public string P1Description { get; set; }
+  }
+}
