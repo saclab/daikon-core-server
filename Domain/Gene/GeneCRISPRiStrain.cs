f956ff71abb8d4f8b6bd70d74632d0f93fefff60
Siddhant Rath <sid@tamu.edu>
2022-07-20 12:10:15 -0500

    Embeded Docs

--- Domain/Gene/GeneCRISPRiStrain.cs
+++ Domain/Gene/GeneCRISPRiStrain.cs
@@ -7,6 +7,15 @@ namespace Domain
 {
   public class GeneCRISPRiStrain
   {
+    /*
+      Define: CRISPR stands for Clustered Regularly Interspaced Short Palindromic Repeats
+      is a genetic engineering technique in molecular biology by which the genomes of 
+      living organisms may be modified. It is based on a simplified version of the bacterial 
+      CRISPR-Cas9 antiviral defense system. By delivering the Cas9 nuclease complexed with 
+      a synthetic guide RNA (gRNA) into a cell, the cell's genome can be cut at a desired 
+      location, allowing existing genes to be removed and/or new ones added in vivo.
+    */
+
     public Guid Id { get; set; }
     public Guid GeneId { get; set; }
     public string GeneAccessionNumber { get; set; }
