e9cadcfb71d680c5f9614ee3b4c4651114b3e63d
Siddhant Rath <sid@tamu.edu>
2023-01-31 20:06:59 -0600

    New Methods for UserAccess

--- Application/Interfaces/IUserAccessor.cs
+++ Application/Interfaces/IUserAccessor.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Threading.Tasks;
+
 namespace Application.Interfaces
 {
-    public interface IUserAccessor
-    {
-        string GetUsername();
-    }
+  public interface IUserAccessor
+  {
+    string GetUsername();
+    Task<Guid> GetUserOrgId();
+    Task<bool> isInRole(string roleName);
+  }
 }
--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -1,21 +1,51 @@
+using System;
+using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Application.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace Infrastructure.Security
 {
   public class UserAccessor : IUserAccessor
   {
     private readonly IHttpContextAccessor _httpContextAccessor;
-    public UserAccessor(IHttpContextAccessor httpContextAccessor)
+    private readonly DataContext _context;
+    public UserAccessor(IHttpContextAccessor httpContextAccessor, DataContext context)
     {
       _httpContextAccessor = httpContextAccessor;
+      _context = context;
     }
     public string GetUsername()
     {
+      return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email);
+    }
 
-      return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+    public async Task<Guid> GetUserOrgId()
+    {
+      Console.WriteLine("Trace");
+      Console.WriteLine(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name));
+      Console.WriteLine(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email));
 
+      var user = await _context.Users
+        .Include(u => u.Org)
+        .FirstOrDefaultAsync(u => u.Email == _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email));
+      return user.Org.Id;
+    }
+
+    public async Task<bool> isInRole(string roleName)
+    {
+      var role = await _context.Roles.FirstOrDefaultAsync(r => r.Name == roleName);
+      var roleId = role.Id;
+
+      var user = await _context.Users
+       .Include(u => u.Org)
+       .FirstOrDefaultAsync(u => u.Email == _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email));
+
+      var roles = await _context.UserRoles.Where((u) => u.UserId == user.Id).Select(r => r.RoleId).ToListAsync();
+      return roles.Contains(roleId);
     }
   }
 }
