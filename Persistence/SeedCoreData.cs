f46f24342664872c524780863628171f42708b28
Siddhant Rath <sid@tamu.edu>
2023-03-21 09:22:50 -0500

    SeedCoreDataCheckFile

--- Persistence/SeedCoreData.cs
+++ Persistence/SeedCoreData.cs
@@ -5,6 +5,7 @@ using Domain;
 using Microsoft.AspNetCore.Identity;
 using Persistence.YAMLDTOs;
 using YamlDotNet.Serialization;
+using System.Security.Cryptography;
 
 namespace Persistence
 {
@@ -14,35 +15,83 @@ namespace Persistence
     private UserManager<AppUser> _userManager;
     private RoleManager<AppRole> _roleManager;
 
+    private string _appRolePath;
+    private string _appUserPath;
+    private string _appAdminPath;
+    private string _appValPath;
+    private string _targetPromotionQuestionPath;
+
+
     public SeedCoreData(DataContext context, UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
       _context = context;
       _userManager = userManager;
       _roleManager = roleManager;
+
+      _appRolePath = "/app/Data/Sample/appRoles.yaml";
+      _appUserPath = "/app/Data/Sample/appUsers.yaml";
+      _appAdminPath = "/app/Data/Sample/appAdmins.yaml";
+      _appValPath = "/app/Data/Sample/appVals.yaml";
+      _targetPromotionQuestionPath = "/app/Data/Sample/targetPromotionQuestions.yaml";
     }
+
+    // Helper
+    public static string GenerateStrongPassword(int length)
+    {
+      const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+      using (var rng = RandomNumberGenerator.Create())
+      {
+        var bytes = new byte[length];
+        rng.GetBytes(bytes);
+        var password = new string(bytes
+            .Select(b => validChars[b % validChars.Length])
+            .ToArray());
+        return password;
+      }
+    }
+
     public async Task SeedCore()
     {
       /* Create Default Roles */
       if (!_roleManager.Roles.Any())
-        await seedAppRoles("/app/Data/Sample/appRoles.yaml");
+      {
+        if (File.Exists(_appRolePath))
+          await seedAppRoles(_appRolePath);
+      }
+
 
       /* Create Default Users */
       if (!_userManager.Users.Any())
-        await seedAppUsers("/app/Data/Sample/appUsers.yaml");
+      {
+        if (File.Exists(_appUserPath))
+          await seedAppUsers(_appUserPath);
+      }
 
       /* Create Default Admins */
-      await seedAdmins("/app/Data/Sample/appAdmins.yaml");
+      if (File.Exists(_appAdminPath))
+      {
+        await seedAdmins(_appAdminPath);
+      }
+
 
       /* Create Basic App Data */
       if (!_context.Questions.Any())
-        await seedTargetPromotionQuestions("/app/Data/Sample/targetPromotionQuestions.yaml");
+      {
+        if (File.Exists(_targetPromotionQuestionPath))
+        { await seedTargetPromotionQuestions(_targetPromotionQuestionPath); }
+      }
 
+
+      /* Create Default AppVals */
       if (!_context.AppVals.Any())
-        await seedAppVals("/app/Data/Sample/appVals.yaml");
-
+      {
+        if (File.Exists(_appValPath))
+        {
+          await seedAppVals(_appValPath);
+        }
+      }
 
       await _context.SaveChangesAsync();
-
     }
 
     private async Task seedTargetPromotionQuestions(string path)
@@ -80,7 +129,8 @@ namespace Persistence
 
       foreach (var user in appUsersYml.Data)
       {
-        await _userManager.CreateAsync(user, "P@ssw0rdTh@TW!llNeverBeUsed=S!gnIn0nly@ll0wedV!@AD");
+        // User sign in is only allowed via SSO. Set a random password that would never be used.
+        await _userManager.CreateAsync(user, GenerateStrongPassword(24));
       }
 
     }
