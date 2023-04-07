f34923e26a14627e09a1d3e041f3937c21a6d034
Siddhant Rath <sid@tamu.edu>
2023-04-07 16:51:51 -0500

    Bgtask

---
+++ Domain/AppBackgroundTask/AppBackgroundTask.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.AppBackgroundTasks
+{
+  public class AppBackgroundTask
+  {
+    // Background Task properties that needs to be tracked
+    public Guid Id { get; set; }
+    public string TaskName { get; set; }
+    public string TaskDescription { get; set; }
+    public string TaskStatus { get; set; }
+    public string TaskType { get; set; }
+    public string TaskModule { get; set; }
+    public string TaskSubModule { get; set; }
+    public string TaskResult { get; set; }
+    public string TaskResultMessage { get; set; }
+    public string TaskResultData { get; set; }
+
+    public DateTime TaskStartTime { get; set; }
+    public DateTime TaskEndTime { get; set; }
+    public DateTime TaskLastUpdated { get; set; }
+    public DateTime TaskCreated { get; set; }
+
+    public string TaskCreatedBy { get; set; }
+
+
+
+  }
+}
